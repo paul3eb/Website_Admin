1572e3d253b71e66d35bef8f0499d39dd536126f
Paul Arsolon <depeddavnor.ictu@deped.gov.ph>
2021-12-01 15:13:17 +0800

    update

--- resources/views/records/order/partials/form.blade.php
+++ resources/views/records/order/partials/form.blade.php
@@ -1,5 +1,21 @@
 
-<div class="col-md-14">
+@foreach ($orders as $data)
+<tr>
+    <td>{{ $data->id }}</td>
+    <td>{{ $data->date }}</td>
+    <td>{{ $data->title }}</td>
+    <td>{{ $data->file }}</td>
+    <td class="project-actions text-left">
+        <a class="btn btn-primary btn-sm "   href="{{ route('order.show', $data->id) }}"><i class="fas fa-folder"></i> View </a>
+        <a class="editIcon btn btn-info btn-sm " id="{{ $data->id }}" data-bs-toggle="modal" data-bs-target="#editOrderModal"><i class="fas fa-pencil-alt"></i> Edit </a>
+        <a class="btn btn-sm btn-danger deleteIcon" id="{{ $data->id }}" ><i class="fas fa-trash"></i> Delete </a></td>            
+</tr>   
+@endforeach
+<tr class="invitation_page_link">
+<td colspan="6" align="right">{{ $orders->links() }}</td>
+</tr>
+
+{{-- <div class="col-md-14">
   <div class="card card-outline card-primary">
     <div>
       <br />
@@ -51,7 +67,7 @@
         <a href="{{ route('dashboard.order') }}"class="btn  btn-danger float-right">Cancel</a>
       </div>
   </div>
-</div>
+</div> --}}
 
 
 
